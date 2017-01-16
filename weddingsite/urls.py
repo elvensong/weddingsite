8dc402339fe7a3583c87e102b02671465de996f1
Dung Vu <dung.vu.eve@gmail.com>
2017-01-16 23:27:02 +0700

    Add homepage

--- weddingsite/urls.py
+++ weddingsite/urls.py
@@ -17,10 +17,12 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
+from . import views
 
 admin.autodiscover()
 
 urlpatterns = [
+    url(r'^$', views.index, name='index'),
     url(r'^album/', include('photologue.urls', namespace='photologue')),
     #url(r'^album/', include('album.urls')),
     url(r'^budget/', include('budget.urls')),
